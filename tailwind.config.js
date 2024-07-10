fee4af9af4d881edabec2d4feb7e4a67fca4482b
Eustus Mwirigi <mwirigieustus97@gmail.com>
2024-07-10 11:22:23 +0300

    ADDING A VITE BOILERPLATE

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: { 'sans': ['Poppins', 'sans-serif'] },
+    },
+  },
+  plugins: [],
+}
+
