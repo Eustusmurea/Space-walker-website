fd7a7cadeb2b6f77314c503e584b0c2e172d923f
Eustus Mwirigi <mwirigieustus97@gmail.com>
2024-07-11 17:01:58 +0300

    chore: Update CSS styles and dimensions for the Destination page

--- src/pages/destination.tsx
+++ src/pages/destination.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import '../App.css';
+import { Link } from 'react-router-dom';
 const Destination = () => {
     return(
         <div className='destination'>
-            <h1>Destination page</h1>
-            <p>Welcome to the destination page</p>
+        <div className='main-content'>
+            <div className='content'>
+                <h1 className='body-xs'>O1 PICK YOUR DESTINATION</h1>
+                <div className='image'>
+                    <img src='Space-walker-website\src\assets\destination\image-moon.png'>
+                    </img>
+                    <div className='destination-heading'>
+                    <nav>
+                        <ul className='navbar-list'>
+                             <li>
+                                <Link to="/moon">01 MOON</Link>
+                            </li>
+                            <li>
+                                 <Link to="/mars">02 MARS</Link>
+                            </li>
+                             <li>
+                                <Link to="/europa">03 EUROPA</Link>
+                            </li>
+                            <li>
+                                <Link to="/titan">04 TITAN</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    
+                    </div>
+
+                </div>
+            </div>
+        </div>
         </div>
     );
 };
