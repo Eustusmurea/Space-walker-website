2191131e9df58d34cccc03a376ee4bb914fdf380
Eustus Mwirigi <mwirigieustus97@gmail.com>
2024-07-15 21:02:37 +0300

    chore: Update CSS styles and dimensions for the Destination page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import '../App.css';
-
+import React from "react";
+import "../App.css";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <div className='hero'>
-      <p className='heading'>
+    <div className="home">
+      <div className="hero">
+        <p className="heading">
           SO, YOU WANT TO TRAVEL TO, <br />
-           <span className='heading-xs'>
-            SPACE
-             </span>
-            </p>
-      <p className='body-xs'>
-        Let's face it; if you want to go to space, you might as well genuinely go <br />
-        to outer space and not hover kind of on the edge of it. Well sit back, <br />
-        and relax because we'll give you a truly out of this world experience!
-      </p>
-      <button> Explore</button>
+          <span className="heading-xs">SPACE</span>
+        </p>
+        <p className="body-xs">
+          Let's face it; if you want to go to space, you might as well genuinely
+          go <br />
+          to outer space and not hover kind of on the edge of it. Well sit back,{" "}
+          <br />
+          and relax because we'll give you a truly out of this world experience!
+        </p>
       </div>
-    </div>    
+      <div>
+        <button className="button"> Explore</button>
+      </div>
+    </div>
   );
 };
 
