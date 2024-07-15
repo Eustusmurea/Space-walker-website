621f06475e5c202f5442edd7228c62fe0d3fa2d3
Eustus Mwirigi <mwirigieustus97@gmail.com>
2024-07-15 18:50:26 +0300

    chore: Update crew page with new image sources and navigation component

--- src/pages/crew.tsx
+++ src/pages/crew.tsx
@@ -5,28 +5,29 @@ import Engineer from './engineer';
 import Pilot from './pilot';
 import Specialist from './specialist';
 import douglas from '../assets/crew/image-douglas-hurley.png';
-import ansari from '../assets/crew/image-anousheh-ansari.png'
+import ansari from '../assets/crew/image-anousheh-ansari.png';
 import victor from '../assets/crew/image-victor-glover.png';
 import mark from '../assets/crew/image-mark-shuttleworth.png';
-import NavCrew from '../components/ui/navCrew';
-
+import DotNav from '../components/ui/navCrew';
 
 import '../App.css';
+
 const Crew = () => {
-    const[imageSrc, setImageSrc] = useState('douglas');
+    const [imageSrc, setImageSrc] = useState(douglas);
     const location = useLocation();
+
     const loadImage = (section: string) => {
-        switch(section){
-            case 'douglas':
+        switch(section) {
+            case 'Commander':
                 setImageSrc(douglas);
                 break;
-            case 'ansari':
+            case 'Mission specialist':
                 setImageSrc(ansari);
                 break;
-            case 'victor':
+            case 'Pilot':
                 setImageSrc(victor);
                 break;
-            case 'mark':
+            case 'Flight Engineer':
                 setImageSrc(mark);
                 break;
             default:
@@ -40,27 +41,27 @@ const Crew = () => {
         loadImage(currentPath);
     }, [location]);
 
-    return(
-        <div className='crew-page'>
-            <div className='title'>
-            <span className="title-number">02</span>MEET YOUR CREW
+    return (
+        <div className="crew-page">
+            <div className="title">
+                <span className="title-number">02</span> MEET YOUR CREW
             </div>
-            <div>   
-            <img src={imageSrc} alt="destination" className="destination-image" />
-          </div>
-                <div>
-                    <NavCrew />
+            <div className="crew-content">
+                <div className="crew-image">
+                    <img src={imageSrc} alt="crew" className="crew-image-element" />
                 </div>
-                <Routes>
-                <Route path="Commander" element={<Commander />} />
-                <Route path="Mission specialist" element={<Specialist />} />
-                <Route path="Pilot" element={<Pilot />} />
-                <Route path="Flight Engineer" element={<Engineer />} />
-                <Route path="/" element={<Commander />} />
-              </Routes>
+                <div className="crew-details">
+                    <Routes>
+                        <Route path="Commander" element={<Commander />} />
+                        <Route path="Mission specialist" element={<Specialist />} />
+                        <Route path="Pilot" element={<Pilot />} />
+                        <Route path="Flight Engineer" element={<Engineer />} />
+                        <Route path="/" element={<Commander />} />
+                    </Routes>
                 </div>
-
-
+                <DotNav />
+            </div>
+        </div>
     );
 };
 
