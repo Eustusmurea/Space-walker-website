1890673b6cb73fca873cf4131e89ae4743ead43c
Eustus Mwirigi <mwirigieustus97@gmail.com>
2024-07-18 18:27:30 +0300

    Update hero margin-left and button placement on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,21 +4,26 @@ import Button from "./../components/ui/button";
 const Home = () => {
   return (
     <div className="home bg-cover">
-      <div className="hero">
-        <p className="heading">
-          SO, YOU WANT TO TRAVEL TO, <br />
-          <span className="heading-xs">SPACE</span>
-        </p>
-        <p className="body-xs">
-          Let's face it; if you want to go to space, you might as well genuinely
-          go <br />
-          to outer space and not hover kind of on the edge of it. Well sit back,{" "}
-          <br />
-          and relax because we'll give you a truly out of this world experience!
-        </p>
-      </div>
-      <div>
-        <Button />
+      <div className="page-display">
+        <div className="page-dims">
+          <div className="hero">
+            <p className="heading">
+              SO, YOU WANT TO TRAVEL TO, <br />
+              <span className="heading-xs">SPACE</span>
+            </p>
+            <p className="body-xs">
+              Let's face it; if you want to go to space, you might as well
+              genuinely go <br />
+              to outer space and not hover kind of on the edge of it. Well sit
+              back, <br />
+              and relax because we'll give you a truly out of this world
+              experience!
+            </p>
+            <div>
+              <Button />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
