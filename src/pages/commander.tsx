ad71e30d9ff5a9dd9ec9a8ee5fe99c22446d4922
Eustus Mwirigi <mwirigieustus97@gmail.com>
2024-07-18 11:52:27 +0300

    Update crew member image dimensions and styles

--- src/pages/commander.tsx
+++ src/pages/commander.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Commander = () => {
   return (
     <div>
       <div>
-      <h1>COMMANDER</h1>
-      <p>DOUGLAS HURLEY</p>
+        <h1 className="heading-n">COMMANDER</h1>
+        <p className="heading-m">DOUGLAS HURLEY</p>
       </div>
-        <div>
-            Douglas Gerald Hurley is an American engineer, former Marine Corps <br />
-            pilot and former NASA astronaut. He launched into space for the third <br />
-            time as commander of Crew Dragon Demo-2. <br />
-            </div>
+      <div>
+        <p className="desktop-body">
+          Douglas Gerald Hurley is an American engineer, former Marine Corps{" "}
+          <br />
+          pilot and former NASA astronaut. He launched into space for the third{" "}
+          <br />
+          time as commander of Crew Dragon Demo-2. <br />
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Commander
+export default Commander;
